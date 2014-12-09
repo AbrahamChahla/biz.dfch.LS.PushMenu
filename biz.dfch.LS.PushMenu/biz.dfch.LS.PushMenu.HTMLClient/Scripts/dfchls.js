10b31cbff9e4cc3b11b11a879c9c4059a3c7af98
Carsten Kreissl <carsten.kreissl@d-fens.net>
2014-12-09 14:49:08 +0100

    Update

--- biz.dfch.LS.PushMenu/biz.dfch.LS.PushMenu.HTMLClient/Scripts/dfchls.js
+++ biz.dfch.LS.PushMenu/biz.dfch.LS.PushMenu.HTMLClient/Scripts/dfchls.js
@@ -58,36 +58,3 @@ function checkMenuOptions(menuoptions) {
 
 }
 
-//obsolete:
-/*
-function updateContainersToPush() {
-    
-        try {
-
-            // Array hold the ContainersToPush
-            var arrContainersToPush = [];
-
-            // Loop thru each LightSwitch page named 'page'
-            $("div[data-role*='page']").each(function() {
-                // Add the page to the array
-                var LightSwitchPage = $(this)[0];
-                arrContainersToPush.push(LightSwitchPage);
-            });
-
-            // Loop thru each LightSwitch header page named 'header'
-            //$("div[data-role*='header']").each(function() {
-            //    // Add the page to the array
-            //    var LightSwitchHeaderPage = $(this)[0];
-            //    arrContainersToPush.push(LightSwitchHeaderPage);
-            //});
-
-            // Set the containersToPush for the menu
-            $('#menu').multilevelpushmenu('option', 'containersToPush', arrContainersToPush);
-        } catch (e) {
-            console.log(e);
-            dfchls.addPushMenu($('#menu'), null);
-        }
-    
-
-}
-*/
